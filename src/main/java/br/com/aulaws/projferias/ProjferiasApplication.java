33e8021d993aa4230ee6401b1cc2f373f850743d
Roussian Gaioso <roussian23@gmail.com>
2021-09-13 16:03:48 -0300

    Projeto Requisiçao de Ferias com WSDL e XSD criados

---
+++ src/main/java/br/com/aulaws/projferias/ProjferiasApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulaws.projferias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjferiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjferiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aulaws/projferias/ProjferiasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aulaws.projferias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjferiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
